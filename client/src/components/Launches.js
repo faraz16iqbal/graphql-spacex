1e47db9f2132e5cb8400c3f28e7516d8d1032035
Faraz Iqbal <faraziqbal@hotmail.co.uk>
2020-11-19 14:12:00 +0530

    Loading

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import LaunchItem from "./LaunchItem";
 import MissionKey from "./MissionKey";
+import { Dimmer, Loader, Segment } from "semantic-ui-react";
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -23,7 +24,14 @@ export class Launches extends Component {
         <MissionKey />
         <Query query={LAUNCHES_QUERY}>
           {({ loading, error, data }) => {
-            if (loading) return <h4>Loading...</h4>;
+            if (loading)
+              return (
+                <Segment>
+                  <Dimmer active inverted>
+                    <Loader inverted content="Loading" />
+                  </Dimmer>
+                </Segment>
+              );
             if (error) console.log(error);
             console.log(data);
             return (
